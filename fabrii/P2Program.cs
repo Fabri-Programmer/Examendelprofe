46546bc159c5d0dcca38496a1fb9e3d701b7b968
Fabri-Programmer <fabrizziopks@hotmail.com>
2024-09-19 09:17:28 -0500

    primer committt

---
+++ fabrii/P2Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string text;
+Console.Write("Presiona (10) para empezar el programa = ");
+text = Console.ReadLine();
+while (text != "F")
+{
+    Console.Write("Cuantos autos ingresaron = ");
+    text = Console.ReadLine();
+    if (text == "0")
+    {
+        Console.WriteLine("\"verde\"");
+    }
+    if (text == "1")
+    {
+        Console.WriteLine("\"verde\"");
+    }
+    if (text == "2")
+    {
+        Console.WriteLine("\"verde\"");
+    }
+    if (text == "3")
+    {
+        Console.WriteLine("\"verde\"");
+    }
+    if (text == "4")
+    {
+        Console.WriteLine("\"Azul\"");
+    }
+    if (text == "5")
+    {
+        Console.WriteLine("\"Azul\"");
+    }
+    if (text == "6")
+    {
+        Console.WriteLine("\"Azul\"");
+    }
+    if (text == "7")
+    {
+        Console.WriteLine("\"rojo");
+    }
+    if (text == "8")
+    {
+        Console.WriteLine("rojo");
+    }
+    if (text == "9")
+    {
+        Console.WriteLine("rojo");
+    }
---
+++ profe/P1Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+double salario, ventas, cantid_a, totalS = 0, TotalT = 0, totalV = 0;
+string text;
+Console.Write("Cual es tu nombre = ");
+text = Console.ReadLine();
+Console.Write("Salario bruto = ");
+salario = double.Parse(Console.ReadLine());
+Console.Write("Cuantas ventas hiciste = ");
+ventas = double.Parse(Console.ReadLine());
+Console.Write("Cuantos años tienes en la empresa = ");
+cantid_a = double.Parse(Console.ReadLine());
+if (ventas <= 600)
+{
+    totalV = salario + (salario * 0.05);
+}
+else if (ventas > 600 && ventas <= 3000)
+{
+    totalV = salario + (salario * 0.07);
+}
+else if (ventas > 3000)
+{
+    totalV = salario + (salario * 0.06) + 300;
+}
+double TotalD = 0;
+if (cantid_a <= 3)
+{
+    TotalD = totalV - (totalV * 0.065);
+}
+else if (cantid_a >= 4)
+{
+    TotalD = totalV - (totalV * 0.040);
+}
+Console.WriteLine("\t\nTu nombre es = " + text);
+Console.WriteLine("\t\nTu sueldo bruto es = " + salario);
+Console.WriteLine("\t\nTu porcentaje de ventas sumadas = " + (totalV - salario));
+Console.WriteLine("\t\nEl total ganado de salario + ventas - impuestos es = " + TotalD);
+Console.WriteLine("\t\ncantidad de años en la empresa = " + cantid_a);
